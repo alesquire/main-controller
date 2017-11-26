062611c6f3a03b44c418d529199d1a86b96faebb
Al <Al@STATION>
2017-11-27 00:53:37 +0400

    add autostop timer

---
+++ Main_Controller/AutostopTimer.cpp
@@ -0,0 +1,27 @@
+// 
+// 
+// 
+
+#include "AutostopTimer.h"
+
+/*class AutostopTimerON : public AutostopTimer
+{
+public:
+	virtual void apply()
+	{
+		//todo : turn on timer
+	}
+};
+const AutostopTimer AutostopTimer::ON = AutostopTimerON();
+
+
+class AutostopTimerOFF : public AutostopTimer
+{
+public:
+	virtual void apply()
+	{
+		//todo : turn on timer
+	}
+};
+const AutostopTimer AutostopTimer::OFF = AutostopTimerOFF();
+*/
