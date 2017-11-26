b3893597ffd309b39d01dfef06bd437abe744be3
Al <Al@STATION>
2017-11-27 01:12:14 +0400

    compilable state with states impletented with references

--- Main_Controller/States.cpp
+++ Main_Controller/States.cpp
@@ -3,7 +3,25 @@
 // 
 
 
-#include "Relays.h"
+#include "States.h"
+States::States(Motor _motorState,
+	Microlift _microliftStates,
+	Relays _relaysState,
+	TonearmState _tonearmState,
+	TonearmButtons _tonearmButtons,
+	SpeedButtons _speedButtons,
+	DiskLed _diskLed,
+	AutostopTimer _autostopTimer)
+{
+	motorState = &_motorState;
+	microliftStates= &_microliftStates;
+	relaysState = &_relaysState;
+	tonearmState = &_tonearmState;
+	tonearmButtons = &_tonearmButtons;
+	speedButtons = &_speedButtons;
+	diskLed = &_diskLed;
+	autostopTimer = &_autostopTimer;
+}
 
 
 
