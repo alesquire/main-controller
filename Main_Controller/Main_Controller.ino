60a6d8e627073c4fb35b75c28de70974f06b4a52
Al <Al@STATION>
2017-11-27 00:43:53 +0400

    Добавьте файлы проекта.

---
+++ Main_Controller/Main_Controller.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Main_Controller.ino
+ Created:	27.11.2017 0:43:47
+ Author:	Al
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
