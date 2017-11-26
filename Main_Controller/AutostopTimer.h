eab0fe8c11b3fcfe9b8e2a2d9b90f0163f79fb03
Al <Al@STATION>
2017-11-27 00:53:37 +0400

    add autostop timer

---
+++ Main_Controller/AutostopTimer.h
@@ -0,0 +1,32 @@
+// AutostopTimer.h
+
+#ifndef _AUTOSTOPTIMER_h
+#define _AUTOSTOPTIMER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "Appliable.h"
+/*
+class AutostopTimer : public Appliable
+{
+public:
+	virtual void init()
+	{
+		//todo - init timer if necessary
+	}
+
+	virtual void apply()
+	{
+		//empty method - should be overridden in child classes with apply behavior
+	}
+
+	static const AutostopTimer ON;
+	static const AutostopTimer OFF;
+};
+*/
+#endif
+
