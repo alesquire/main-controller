d5638094b1b6b31364b012dff802a433513dca3f
Al <Al@STATION>
2017-12-12 00:55:51 +0400

    small changes in documentation

--- Main_Controller/Main_Controller.ino
+++ Main_Controller/Main_Controller.ino
@@ -11,14 +11,18 @@
 Main documentation
 _____________________________________________________
 
-	Timers:
+Timers:
 	* Timer0 is used for autostop delayed function call. Autostop is ititiated not immediately after sensor is activated, but after interval for two reasons:
 		- to simplify sensor position adjustment- sensor detects autostop somewhere near autostop position,
 		- to play recordings in autostop trask (St.Peppers Lonly Hearts Club Band
 	* Timer1 is used to periodicaly check tonearm analog controls that can't produce events (joystick, damper and antiscate regulators) and update solenoid output voltage. 
 
-	Buttons have HIGH voltage in board input when is not presed and LOW when buttn is pressed. 
+Buttons and switches
+	
+	* Buttons have HIGH voltage in board input when is not presed and LOW when buttn is pressed. 
 	Main event for buttons is presion, so- interrupt FALLING
+
+	* Microlift sensors produce HIGH level when tonearm is up and LOW when it is down
 
 */
 
