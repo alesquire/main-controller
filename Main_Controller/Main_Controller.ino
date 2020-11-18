ee62dffb15f3fffb6d13ceaf8841dd7e77d31bd9
Al <Al@STATION>
2020-11-18 20:31:02 +0400

    еще небольшие изменеия

--- Main_Controller/Main_Controller.ino
+++ Main_Controller/Main_Controller.ino
@@ -5,29 +5,27 @@
 */
 
 
-//#include "SensorsState.h"
+#include "SensorsState.h"
 #include "DebugFunctions.h"
-//#include "Stroboscope.h"
-//#include "StateProcessor.h"
-//#include "SensorsState.h"
+#include "Stroboscope.h"
+#include "StateProcessor.h"
+#include "SensorsState.h"
 
 
 void setup() 
 {
-	Serial.print("test1\n");
 //#if defined (DEBUG)	
 	Serial.begin(115200);//debug 
 //#endif
 	debug("start\n") ;//debug
-	//SensorsState::sensorsState.init();
-	//StateProcessor::stateProcessor.init();
+	SensorsState::sensorsState.init();
+	StateProcessor::stateProcessor.init();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() 
 {
-	Serial.print("test\n");
+	debug("-\n");
 	//SensorsState::sensorsState.compare();
 	delay(100);
 }
-
